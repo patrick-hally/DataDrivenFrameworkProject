cd55507ab20ffb4a015f046e17f68e1e2633846d
patrick-hally <pahally1@hotmail.com>
2020-06-24 15:31:53 +0100

    Committing screenshot capabilities

--- src/test/java/listeners/CustomListeners.java
+++ src/test/java/listeners/CustomListeners.java
@@ -1,10 +1,14 @@
 package listeners;
 
+import java.io.IOException;
+
 import org.testng.ISuite;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.testng.Reporter;
+
+import utilities.TestUtil;
 
 public class CustomListeners implements ITestListener{
 
@@ -26,10 +30,17 @@ public class CustomListeners implements ITestListener{
 		public void onTestFailure(ITestResult arg0) {
 		
 			System.setProperty("org.uncommons.reportng.escape-output", "false");
-			Reporter.log("Capturingscreenshot");
-			Reporter.log("<a target=\"_blank\" href=\"C:\\Users\\Patrick\\Pictures\\Capture10.png\">Screenshot</a>");
+			try {
+				TestUtil.captureScreenshot();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			Reporter.log("Click to see screenshot");
+			Reporter.log("<a target=\"_blank\" href="+TestUtil.screenshotName+">Screenshot</a>");
 			Reporter.log("<br>");
-			Reporter.log("<a target=\"_blank\" href=\"C:\\Users\\Patrick\\Pictures\\Capture10.png\"><img src=\"C:\\Users\\Patrick\\Pictures\\Capture10.png\" height=200 width=200></img></a>");
+			Reporter.log("<br>");
+			Reporter.log("<a target=\"_blank\" href="+TestUtil.screenshotName+"><img src="+TestUtil.screenshotName+" height=200 width=200></img></a>");
 		
 			
 		}
